5580ef5844931ab6e1b213e255b120c4b9fb8f75
lorenz <lorenz.boss@bbzbl-it.ch>
2024-05-27 16:14:33 +0200

    add header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AppAuthService } from '../../service/app.auth.service';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(
+    private authService: AppAuthService,
+    public oauthService: OAuthService
+  ) {}
 
+  login() {
+    console.log('login');
+
+    this.authService.login();
+  }
 }
