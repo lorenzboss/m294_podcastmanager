247de823743b2b863130ac813d9cc041af8b4b50
lorenz <lorenz.boss@bbzbl-it.ch>
2024-05-27 16:15:58 +0200

    add all necessary configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,94 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthConfig, OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HttpXSRFInterceptor } from './interceptor/http.csrf.interceptor';
+import { AppAuthService } from './service/app.auth.service';
+import { IsInRoleDirective } from './dir/is.in.role.dir';
+import { IsInRolesDirective } from './dir/is.in.roles.dir';
+import { environment } from '../environments/environment.prod';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientModule,
+  HttpClientXsrfModule,
+} from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NoAccessComponent } from './pages/no-access/no-access.component';
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/ILV',
+  requireHttps: false,
+  redirectUri: environment.frontendBaseUrl,
+  postLogoutRedirectUri: environment.frontendBaseUrl,
+  clientId: 'podcastmanager',
+  scope: 'openid profile roles offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+  requestAccessToken: true,
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshTimeout: 500,
+  clearHashAfterLogin: true,
+};
+
+export function storageFactory(): OAuthStorage {
+  return sessionStorage;
+}
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    IsInRoleDirective,
+    IsInRolesDirective,
+    HeaderComponent,
+    TestPageComponent,
+    HomepageComponent,
+    NoAccessComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    OAuthModule.forRoot({ resourceServer: { sendAccessToken: true } }),
+    HttpClientModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'X-XSRF-TOKEN',
+    }),
   ],
-  providers: [provideAnimationsAsync()],
+  providers: [
+    provideAnimationsAsync(),
+    { provide: AuthConfig, useValue: authConfig },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpXSRFInterceptor, multi: true },
+    {
+      provide: OAuthStorage,
+      useFactory: storageFactory,
+    },
+    Location,
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(authService: AppAuthService) {
+    authService.initAuth().finally();
+  }
+}
