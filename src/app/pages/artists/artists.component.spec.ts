51026f3b4536e804ea6d3c385a4f4328bc61dcd6
lorenz <lorenz.boss@bbzbl-it.ch>
2024-05-31 10:49:30 +0200

    add artist component test

--- src/app/pages/artists/artists.component.spec.ts
+++ src/app/pages/artists/artists.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtistsComponent } from './artists.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { IsInRoleDirective } from '../../dir/is.in.role.dir';
+import { AuthConfig, OAuthModule } from 'angular-oauth2-oidc';
+import { authConfig } from '../../app.module';
 
 describe('ArtistsComponent', () => {
   let component: ArtistsComponent;
@@ -8,10 +13,15 @@ describe('ArtistsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ArtistsComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        MatTableModule,
+        HttpClientModule,
+        OAuthModule.forRoot({ resourceServer: { sendAccessToken: true } }),
+      ],
+      providers: [{ provide: AuthConfig, useValue: authConfig }],
+      declarations: [ArtistsComponent, IsInRoleDirective],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ArtistsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
