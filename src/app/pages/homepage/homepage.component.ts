be05d276a8a53fe8385c18b8ba9e6e7e6259837e
lorenz <lorenz.boss@bbzbl-it.ch>
2024-05-31 11:34:08 +0200

    remove logic from homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,50 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { WritersService } from '../../service/writers.service';
-import { CreateWriter, Writer } from '../../data/writer';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent implements OnInit {
-  constructor(private writerService: WritersService) {}
-
-  ngOnInit() {
-    this.saveWriter();
-  }
-
-  saveWriter() {
-    const writer: CreateWriter = { firstname: 'John', lastname: 'Boss' };
-    this.writerService.saveWriter(writer).subscribe(() => {
-      this.getWriters();
-    });
-  }
-
-  getWriters() {
-    this.writerService.getWriters().subscribe((writers) => {
-      console.log(writers);
-
-      writers.forEach((writer) => {
-        this.updateWriter(writer);
-      });
-    });
-  }
-
-  updateWriter(writer: Writer) {
-    writer = { ...writer, firstname: 'Lorenz' };
-    this.writerService.updateWriter(writer).subscribe((writer) => {
-      console.log(writer);
-      this.deleteWriter(writer);
-    });
-  }
-
-  deleteWriter(writer: Writer) {
-    this.writerService.deleteWriter(writer).subscribe(() => {
-      console.log('Writer deleted');
-      this.writerService.getWriters().subscribe((writers) => {
-        console.log(writers);
-      });
-    });
-  }
-}
+export class HomepageComponent {}
